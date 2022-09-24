81f2a070af86ed3a5c104a37a30f4baf2712ccc5
Abhishek Singh <abhisheksingh16102000@bbdu.ac.in>
2022-09-25 01:36:57 +0530

    accordion code updated

--- spotify.js
+++ spotify.js
@@ -2,3 +2,14 @@ function cancel(){
     let top_nav = document.getElementById('top-nav')
      top_nav.style.display = "none"
 }
+
+
+
+let accordion = document.querySelectorAll('.accordion-content')
+console.log(accordion)
+
+for(let i=0; accordion.length;i++){
+    accordion[i].addEventListener('click',function(){
+        this.classList.toggle('active') 
+    })
+}
